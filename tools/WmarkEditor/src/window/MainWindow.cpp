5295571fe0b9c8eeec45d358909aebdeec9bc445
meilj <e086402@163.com>
2019-09-01 21:31:14 +0800

    add ReplaceDlg

--- tools/WmarkEditor/src/window/MainWindow.cpp
+++ tools/WmarkEditor/src/window/MainWindow.cpp
@@ -8,6 +8,7 @@
 
 #include "../view/TextEditor.h"
 #include "MainWindow.h"
+#include "common/TextDef.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace CSL {
@@ -21,6 +22,22 @@ MainWindow::MainWindow(int w, int h, const char* t) : Fl_Double_Window(w, h, t),
 {
 	m_menuBar->add("Load...", 0, &load_cb, &m_cmdFunc_load);
 	m_menuBar->add("Save...", 0, &save_cb, &m_cmdFunc_save);
+	m_menuBar->add("Replace...", 0, &replace_cb, this);
+	
+	m_replaceDlg = std::make_shared<Fl_Window>(300, 105, "Replace");
+	m_replaceFind = std::make_shared<Fl_Input>(80, 10, 210, 25, "Find:");
+	m_replaceFind->align(FL_ALIGN_LEFT);
+	m_replaceWith = std::make_shared<Fl_Input>(80, 40, 210, 25, "Replace:");
+	m_replaceWith->align(FL_ALIGN_LEFT);
+	m_replaceAll = std::make_shared<Fl_Button>(10, 70, 90, 25, "Replace All");
+	m_replaceNext = std::make_shared<Fl_Return_Button>(105, 70, 120, 25, "Replace Next");
+	m_replaceCancel = std::make_shared<Fl_Button>(230, 70, 60, 25, "Cancel");
+	m_replaceDlg->end();
+	m_replaceDlg->set_non_modal();
+	m_replaceAll->callback((Fl_Callback*)replace_all_cb, this);
+	m_replaceNext->callback((Fl_Callback*)replace_next_cb, this);
+	m_replaceCancel->callback((Fl_Callback*)replace_cancel_cb, this);
+	
 	this->resizable(*m_textEditor);
 }
 
@@ -34,6 +51,36 @@ std::shared_ptr<TextEditor> MainWindow::get_TextEditor() noexcept
 	return m_textEditor;
 }
 
+void MainWindow::show_ReplaceDialog(bool bShow) noexcept
+{
+	if (bShow)
+		m_replaceDlg->show();
+	else
+		m_replaceDlg->hide();
+}
+
+void MainWindow::set_ReplacePara(bool bRepalceAll)
+{
+	const char *find = m_replaceFind->value();
+	const char *replace = m_replaceWith->value();
+	if (find[0] == '\0')
+	{
+		m_replaceDlg->show();
+		return;
+	}
+	m_replaceDlg->hide();
+
+	ReplacePara para;
+	para.nPos = -1;
+	if(!bRepalceAll)
+		para.nPos = m_textEditor->insert_position();
+	para.strFind = find;
+	para.strReplace = replace;
+	if (m_cmdFunc_replace != nullptr && !m_cmdFunc_replace(std::make_any<ReplacePara>(para))) {
+		fl_alert("Error in repalce file!");
+	}
+}
+
 //commands
 void MainWindow::set_LoadCommand(CommandFunc&& cf)
 {
@@ -43,6 +90,11 @@ void MainWindow::set_LoadCommand(CommandFunc&& cf)
 void MainWindow::set_SaveCommand(CommandFunc&& cf)
 {
 	m_cmdFunc_save = std::move(cf);
+}
+
+void MainWindow::set_ReplaceCommand(CommandFunc&& cf)
+{
+	m_cmdFunc_replace = std::move(cf);
 }
 
 //callbacks
@@ -72,6 +124,63 @@ void MainWindow::save_cb(Fl_Widget*, void* v)
 		}
 	}
 	return;
+}
+
+void MainWindow::replace_cb(Fl_Widget*, void* v)
+{
+	MainWindow* e = (MainWindow*)v;
+	e->show_ReplaceDialog(true);
+}
+
+void MainWindow::replace_all_cb(Fl_Widget*, void* v)
+{
+	MainWindow* e = (MainWindow*)v;
+	e->set_ReplacePara(true);
+	/*const char *find = m_replaceFind->value();
+	const char *replace = m_replaceWith->value();
+	if (find[0] == '\0')
+	{
+		m_replaceDlg->show();
+	}
+	 m_replaceDlg->hide();
+	
+	ReplacePara para;
+	para.pos = -1;
+	para.find = find;
+	para.replace = replace;
+	CommandFunc& cmdFunc = *((CommandFunc*)v);
+	if (cmdFunc != nullptr && !cmdFunc(std::make_any<ReplacePara>(para))) {
+		fl_alert("Error in repalce file!");
+	}*/
+}
+
+void MainWindow::replace_next_cb(Fl_Widget*, void* v)
+{
+	MainWindow* e = (MainWindow*)v;
+	e->set_ReplacePara(false);
+	/*const char *find = m_replaceFind->value();
+	const char *replace = m_replaceWith->value();
+	if (find[0] == '\0')
+	{
+		m_replaceDlg->show();
+	}
+	m_replaceDlg->hide();
+	int pos = m_textEditor->insert_position();
+
+	ReplacePara para;
+	para.pos = pos;
+	para.find = find;
+	para.replace = replace;
+	CommandFunc& cmdFunc = *((CommandFunc*)v);
+	if (cmdFunc != nullptr && !cmdFunc(std::make_any<ReplacePara>(para))) {
+		fl_alert("Error in repalce file!");
+	}*/
+}
+
+void MainWindow::replace_cancel_cb(Fl_Widget*, void* v)
+{
+	MainWindow* e = (MainWindow*)v;
+	e->show_ReplaceDialog(false);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- tools/WmarkEditor/src/window/MainWindow.h
+++ tools/WmarkEditor/src/window/MainWindow.h
@@ -23,10 +23,13 @@ public:
 
 //properties
 	std::shared_ptr<TextEditor> get_TextEditor() noexcept;
+	void show_ReplaceDialog(bool bShow) noexcept;
+	void set_ReplacePara(bool bReplaceAll);
 
 //commands
 	void set_LoadCommand(CommandFunc&& cf);
 	void set_SaveCommand(CommandFunc&& cf);
+	void set_ReplaceCommand(CommandFunc&& cf);
 
 //notifications
 	PropertyNotification get_Notification();
@@ -35,15 +38,28 @@ private:
 //callbacks
 	static void load_cb(Fl_Widget*, void* v);
 	static void save_cb(Fl_Widget*, void* v);
+	static void replace_cb(Fl_Widget*, void* v);
+	static void replace_all_cb(Fl_Widget*, void* v);
+	static void replace_next_cb(Fl_Widget*, void* v);
+	static void replace_cancel_cb(Fl_Widget*, void*v);
+	
 
 private:
 //commands
 	CommandFunc m_cmdFunc_load;
 	CommandFunc m_cmdFunc_save;
+	CommandFunc m_cmdFunc_replace;
 
 //UI
 	std::shared_ptr<TextEditor> m_textEditor;
 	std::shared_ptr<Fl_Menu_Bar> m_menuBar;
+
+	std::shared_ptr<Fl_Window> m_replaceDlg;
+	std::shared_ptr<Fl_Input> m_replaceFind;
+	std::shared_ptr<Fl_Input> m_replaceWith;
+	std::shared_ptr<Fl_Button> m_replaceAll;
+	std::shared_ptr<Fl_Return_Button> m_replaceNext;
+	std::shared_ptr<Fl_Button> m_replaceCancel;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
